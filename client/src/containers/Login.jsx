90a52df6f345c665778f9b6a9904a842e40b8e88
Divyansh Srivastavae <divyanshsrivastava215@gmail.com>
2024-01-04 15:32:28 +0530

    firebase authentications

--- client/src/containers/Login.jsx
+++ client/src/containers/Login.jsx
@@ -5,11 +5,30 @@ import { FaEnvelope, FaLock, FcGoogle } from "../assets/icons/index";
 import { motion } from "framer-motion";
 import { buttonClick } from "../animation";
 
+import {GoogleAuthProvider, getAuth,signInWithPopup} from 'firebase/auth'
+import {app} from '../config/firebase.config'
+
+
 const Login = () => {
   const [userEmail, setUserEmail] = useState("");
   const [isSignUp, setisSignUp] = useState(false);
   const [password, setPassword] = useState("");
   const [confirm_password, setConfirm_password] = useState("");
+
+  const firebaseAuth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const loginWithGoogle = async () => {
+    await signInWithPopup(firebaseAuth,provider).then(userCred => {
+      firebaseAuth.onAuthStateChanged(cred => {
+        if(cred){
+          cred.getIdToken().then(token => {
+            console.log(token)
+          })
+        }
+      })
+    })
+  };
 
   return (
     <div className="w-screen h-screen relative overflow-hidden flex">
@@ -21,7 +40,8 @@ const Login = () => {
       />
 
       {/* content box */}
-      <div className="flex flex-col items-center bg-lightOverlay w-[80%] md:w-508 h-full z-10 backdrop-blur-md p-4 px-4 py-12 gap-6">
+      {/* <div className="flex flex-col items-center bg-lightOverlay w-[80%] md:w-508 h-full z-10 backdrop-blur-md p-4 px-4 py-12 gap-6"> */}
+      <div className="flex flex-col items-center bg-opacity-50 bg-black w-80p md:w-508 h-full z-10 backdrop-blur-md p-4 px-4 py-12 gap-6 rounded-lg shadow-lg">
         {/* logo section */}
         <div className="flex items-center justify-start gap-4 w-full">
           <img src={logo} alt="" className="w-8" />
@@ -32,7 +52,9 @@ const Login = () => {
         <p className="text-3xl font-semibold text-wheadingColor">
           Welcome Back
         </p>
-        <p className="text-xl text-wtextColor -mt-6">{isSignUp ? "Sign Up":"Sign In"} with following</p>
+        <p className="text-xl text-wtextColor -mt-6">
+          {isSignUp ? "Sign Up" : "Sign In"} with following
+        </p>
 
         {/* {input section} */}
         <div className="w-full flex flex-col items-center justify-center gap-6 px-4 md:px-12 py-4">
@@ -90,24 +112,36 @@ const Login = () => {
 
           {/* button */}
           {isSignUp ? (
-            <motion.button {...buttonClick} className="w-full px-4 py-2 rounded-md font-semibold bg-yellow-400 cursor-pointer text-black text-xl capitalize hover:bg-yellow-500 transition-all duration-150">Sign Up</motion.button>
+            <motion.button
+              {...buttonClick}
+              className="w-full px-4 py-2 rounded-md font-semibold bg-yellow-400 cursor-pointer text-black text-xl capitalize hover:bg-yellow-500 transition-all duration-150"
+            >
+              Sign Up
+            </motion.button>
           ) : (
-            <motion.button {...buttonClick} className="w-full px-4 py-2 rounded-md font-semibold bg-yellow-400  text-black hover:bg-yellow-500 cursor-pointer text-xl capitalize  transition-all duration-150">Sign In</motion.button>
+            <motion.button
+              {...buttonClick}
+              className="w-full px-4 py-2 rounded-md font-semibold bg-yellow-400  text-black hover:bg-yellow-500 cursor-pointer text-xl capitalize  transition-all duration-150"
+            >
+              Sign In
+            </motion.button>
           )}
-
         </div>
 
-            <div className="flex items-center justify-between gap-16">
-              <div className="w-24 h-[1px] rounded-md bg-white"></div>
-              <p className="text-white">or</p>
-              <div className="w-24 h-[1px] rounded-md bg-white"></div>
-            </div>
-            
-            <motion.div {...buttonClick} className="flex items-center justify-center px-20 py-2 bg-cardOverlay backdrop-blur-md cursor-pointer rounded-3xl gap-4">
-              <FcGoogle className="text-3xl "/>
-              <p className="capitalize text-base text-white">Sign-in With Google</p>
-            </motion.div>
+        <div className="flex items-center justify-between gap-16">
+          <div className="w-24 h-[1px] rounded-md bg-white"></div>
+          <p className="text-white">or</p>
+          <div className="w-24 h-[1px] rounded-md bg-white"></div>
+        </div>
 
+        <motion.div
+          {...buttonClick}
+          className="flex items-center justify-center px-20 py-2 bg-cardOverlay backdrop-blur-md cursor-pointer rounded-3xl gap-4"
+          onClick={loginWithGoogle}
+        >
+          <FcGoogle className="text-3xl " />
+          <p className="capitalize text-base text-white">Sign-in With Google</p>
+        </motion.div>
       </div>
     </div>
   );
